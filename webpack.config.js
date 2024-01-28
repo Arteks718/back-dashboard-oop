4355264863539d300d8403891b096a68201db984
Arteks718 <artexbro.official@gmail.com>
2024-01-28 23:20:12 +0200

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,53 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
+const path = require("path");
 
-const isProduction = process.env.NODE_ENV == 'production';
-
+const isProduction = process.env.NODE_ENV == "production";
 
 const config = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-    },
-    plugins: [
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
 
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    fallback: {
+      crypto: false,
+      fs: false,
+      net: false,
+      stream: false,
+      querystring: false,
+      http: false,
+      os: false,
+      zlib: false,
+      async_hooks: false,
     },
-    resolve: {
-      fallback: {
-        "crypto": false,
-        "fs": false,
-        "net": false,
-        "stream": false,
-        "querystring": false,
-        "http": false,
-        "os": false,
-        "zlib": false,
-      }
-    }
+  },
 };
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
 };
